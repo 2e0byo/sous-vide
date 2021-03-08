761be338bd773cfc4ca03cf3b9a366ebe342567e
John Maximilian <2e0byo@gmail.com>
2021-03-08 15:14:24 +0000

    in class

--- firmware/hal.py
+++ firmware/hal.py
@@ -29,89 +29,104 @@ rot_left = settablePin(35, settablePin.IN, settablePin.PULL_UP)
 rot_right = settablePin(34, settablePin.IN, settablePin.PULL_UP)
 
 
-font = {
-    " ": 0,
-    "0": 63,
-    "1": 6,
-    "2": 91,
-    "3": 79,
-    "4": 102,
-    "5": 109,
-    "6": 125,
-    "7": 7,
-    "8": 127,
-    "9": 111,
-    "a": 119,
-    "b": 124,
-    "c": 57,
-    "d": 94,
-    "e": 121,
-    "f": 113,
-    "g": 61,
-    "h": 118,
-    "i": 48,
-    "j": 14,
-    "k": 112,
-    "l": 56,
-    "m": 84,
-    "n": 84,
-    "o": 92,
-    "p": 115,
-    "q": 103,
-    "r": 80,
-    "s": 109,
-    "t": 120,
-    "u": 28,
-    "v": 28,
-    "w": 28,
-    "x": 64,
-    "y": 110,
-    "z": 91,
-}
-chars = bytearray(4)
-digit_index = 0
-dim = 1
-count = 0
+class SevenSegment:
 
+    font = {
+        " ": 0,
+        "0": 63,
+        "1": 6,
+        "2": 91,
+        "3": 79,
+        "4": 102,
+        "5": 109,
+        "6": 125,
+        "7": 7,
+        "8": 127,
+        "9": 111,
+        "a": 119,
+        "b": 124,
+        "c": 57,
+        "d": 94,
+        "e": 121,
+        "f": 113,
+        "g": 61,
+        "h": 118,
+        "i": 48,
+        "j": 14,
+        "k": 112,
+        "l": 56,
+        "m": 84,
+        "n": 84,
+        "o": 92,
+        "p": 115,
+        "q": 103,
+        "r": 80,
+        "s": 109,
+        "t": 120,
+        "u": 28,
+        "v": 28,
+        "w": 28,
+        "x": 64,
+        "y": 110,
+        "z": 91,
+    }
 
-# consider using stm to edit the gpio register in one step.
-@micropython.native
-def _disp(p):
-    global digit_index, count
-    count += 1
-    if count == dim:
-        count = 0
-    if count > 1:
+    def __init__(self, seg, digits):
+        self.seg = seg
+        self.digits = digits
+        self.chars = bytearray(4)
+        self.digit_index = 0
+        self.dim = 1
+        self.count = 0
+
+        self.disp_timer = Timer(0)
+        self.disp_timer.init(period=3, mode=Timer.PERIODIC, callback=self._disp)
+        self.display("ave ")
+
+    # consider using stm to edit the gpio register in one step.
+    @micropython.native
+    def _disp(self, p):
+        self.count += 1
+        if self.count == self.dim:
+            self.count = 0
+        if self.count > 1:
+            for i in range(8):
+                self.seg[i].val = 0
+            return
+
+        self.digits[self.digit_index].val = 0
+        self.digit_index += 1
+        self.digit_index &= 3
         for i in range(8):
-            seg[i].val = 0
-        return
+            self.seg[i].val = self.chars[self.digit_index] >> i & 1
+        self.digits[self.digit_index].val = 1
 
-    digits[digit_index].val = 0
-    digit_index += 1
-    digit_index &= 3
-    for i in range(8):
-        seg[i].val = chars[digit_index] >> i & 1
-    digits[digit_index].val = 1
-
-
-def display(s):
-    global chars
-    i = 0
-    for char in str(s):
-        if char == ".":
-            if i:
-                chars[i - 1] |= 1 << 7
-                continue
+    def display(self, s):
+        i = 0
+        for char in str(s):
+            if char == ".":
+                if i:
+                    self.chars[i - 1] |= 1 << 7
+                    continue
+                else:
+                    self.chars[i] = 1 << 7
             else:
-                chars[i] = 1 << 7
-        else:
-            chars[i] = font[char]
+                self.chars[i] = self.font[char]
 
-        i += 1
-        if i == 4:
-            break
+            i += 1
+            if i == 4:
+                break
+
+    @property
+    def brightness(self):
+        return 5 - self.dim
+
+    @brightness.setter
+    def brightness(self, br):
+        if br > 4 or br < 0:
+            raise ValueError("Brightness is between 4 and 0")
+        self.dim = 5 - br
+        self.count = 0
 
 
-disp_timer = Timer(0)
-disp_timer.init(period=3, mode=Timer.PERIODIC, callback=_disp)
-display("ave ")
+seven_seg = SevenSegment(seg, digits)
