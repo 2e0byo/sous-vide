064be5438c78da4e17fa249e609c6eb1c3bde94c
John Maximilian <2e0byo@gmail.com>
2021-03-08 18:33:38 +0000

    pre viper

--- firmware/hal.py
+++ firmware/hal.py
@@ -17,10 +17,9 @@ relay_pin = settablePin(13, settablePin.OUT)
 
 seg = (27, 26, 33, 25, 14, 17, 16, 32)
 # a, b, c, d, e, f, g,  dp
-seg = tuple((settablePin(i, settablePin.OPEN_DRAIN) for i in seg))
+# seg = tuple((settablePin(i, settablePin.OPEN_DRAIN) for i in seg))
 
 digits = (18, 22, 21, 19)
-digits = tuple((settablePin(i, settablePin.OPEN_DRAIN) for i in digits))
 
 
 sensor = settablePin(15)
@@ -70,37 +69,61 @@ class SevenSegment:
         "x": 64,
         "y": 110,
         "z": 91,
+        "-": 64,
     }
 
     def __init__(self, seg, digits):
         self.seg = seg
+        self.seg_pins = tuple((settablePin(i, settablePin.OPEN_DRAIN) for i in seg))
         self.digits = digits
+
+        self.digits_pins = tuple(
+            (settablePin(i, settablePin.OPEN_DRAIN) for i in digits)
+        )
         self.chars = bytearray(4)
         self.digit_index = 0
         self.dim = 1
         self.count = 0
 
+        self._compute_masks()
+
         self.disp_timer = Timer(0)
-        self.disp_timer.init(period=3, mode=Timer.PERIODIC, callback=self._disp)
+        self.disp_timer.init(period=5, mode=Timer.PERIODIC, callback=self._disp)
         self.display("ave ")
+
+    def _compute_masks(self):
+        self.font_masks = {}
+        for k, v in self.font.items():
+            lower_mask = 0
+            upper_mask = 0
+            for i in range(8):
+                if self.seg[i] < 32:
+                    if v & 1 << i:
+                        lower_mask |= 1 << self.seg[i]
+                else:
+                    if v & 1 << i:
+                        upper_mask |= 1 << self.seg[i]
+            self.font_masks[k] = (lower_mask, upper_mask)
 
     # consider using stm to edit the gpio register in one step.
     @micropython.native
     def _disp(self, p):
-        self.count += 1
-        if self.count == self.dim:
-            self.count = 0
-        if self.count > 1:
-            for i in range(8):
-                self.seg[i].val = 0
-            return
+        irq = disable_irq()
+        # self.count += 1
+        # if self.count == self.dim:
+        #     self.count = 0
+        # if self.count > 1:
+        #     for i in range(8):
+        #         self.seg_pins[i].val = 0
+        #     return
 
-        self.digits[self.digit_index].val = 0
+        self.digits_pins[self.digit_index].val = 0
         self.digit_index += 1
         self.digit_index &= 3
         for i in range(8):
-            self.seg[i].val = self.chars[self.digit_index] >> i & 1
-        self.digits[self.digit_index].val = 1
+            self.seg_pins[i].val = self.chars[self.digit_index] >> i & 1
+        self.digits_pins[self.digit_index].val = 1
+        enable_irq(irq)
 
     def display(self, s):
         i = 0
@@ -117,6 +140,7 @@ class SevenSegment:
             i += 1
             if i == 4:
                 break
+        self.count = 0
 
     @property
     def brightness(self):
@@ -147,32 +171,15 @@ class EncoderTimed(object):
             handler=self.y_callback, trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING
         )
 
+    @micropython.native
     def x_callback(self, line):
         self.forward = self.pin_x.value() ^ self.pin_y.value() ^ self.reverse
         self._pos += 1 if self.forward else -1
-        self.tprev = self.tlast
-        self.tlast = utime.ticks_us()
 
+    @micropython.native
     def y_callback(self, line):
         self.forward = self.pin_x.value() ^ self.pin_y.value() ^ self.reverse ^ 1
         self._pos += 1 if self.forward else -1
-        self.tprev = self.tlast
-        self.tlast = utime.ticks_us()
-
-    @property
-    def rate(self):  # Return rate in edges per second
-        if not self.tprev:
-            return 0
-        irq = disable_irq()
-        tprev = self.tprev
-        tlast = self.tlast
-        enable_irq(irq)
-        if utime.ticks_diff(utime.ticks_us(), tlast) > 2000000:  # It's stopped
-            result = 0.0
-        else:
-            result = 1000000.0 / (utime.ticks_diff(tlast, tprev))
-        result *= self.scale
-        return result if self.forward else -result
 
     @property
     def position(self):
@@ -182,6 +189,51 @@ class EncoderTimed(object):
         self._pos = 0
 
 
+seven_seg = SevenSegment(seg, digits)
 encoder = EncoderTimed(rot_left, rot_right, False, 1)
 
-seven_seg = SevenSegment(seg, digits)
+# while True:
+#     seven_seg.display("{:4}".format(encoder.position))
+#     utime.sleep(0.5)
+
+
+@micropython.viper
+def set_gpio(mask: int):
+    GPIO_BASE = ptr32(0x3F404000)  # noqa
+    GPIO_BASE[1] = mask
+
+
+@micropython.viper
+def unset_gpio(mask: int):
+    GPIO = ptr32(0x3FF44004)
+    GPIO[2] = mask
+
+
+@micropython.viper
+def get_gpios() -> int:
+    GPIO = ptr32(0x3FF44004)
+    return int(GPIO_BASE[0x8])
+
+
+@micropython.viper
+def viperbop():
+    GPIO = ptr32(0x3FF44004)
+    while True:
+        GPIO[1] = 1 << 22
+        GPIO[2] = 1 << 22
+
+
+@micropython.viper
+def lower(gs):
+    GPIO = ptr32(0x3FF44004)
+    mask = 0x5ECE0000
+    GPIO[1] = int(gs & mask)
+    GPIO[2] = int(~gs & mask)
+
+
+# @micropython.viper
+# def upper(gs):
+#     GPIO = ptr32(0x3FF44010)
+#     mask = 0x6
+#     GPIO[1] = int(gs & mask)
+#     GPIO[2] = int(~gs & mask)
