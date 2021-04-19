cee8e7735c29acd15de22776658819bfc7ba0dc7
John Maximilian <2e0byo@gmail.com>
2021-04-19 19:31:09 +0100

    typo

--- firmware/api.py
+++ firmware/api.py
@@ -151,7 +151,7 @@ def autotune_status(req, resp):
 
 @app.route(re.compile("^/api/hal/temp/window/[0-9]*"), methods=["PUT"])
 def set_temp_window(req, resp):
-    val = int(req.uril_match.group(1))
+    val = int(req.url_match.group(1))
     if val < 1:
         raise Exception("Window must be at least 1 long!")
     hal.avg = val
